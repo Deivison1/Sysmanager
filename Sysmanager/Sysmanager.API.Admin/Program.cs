d2a29519fe12e6771421463ad94bedee9c2669ed
Deivison1 <santosdeivison480@gmail.com>
2022-06-20 20:29:37 -0300

    Atualizando Sysmanager Subindo arquivos

---
+++ Sysmanager/Sysmanager.API.Admin/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sysmanager.API.Admin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Sysmanager/Sysmanager.Application/Contracts/Users/Request/UserPostRequest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sysmanager.Application.Contracts.Users.Request
+{
+    /// <summary>
+    /// classe responsavel de contrato de requisicao para cadastrar um novo usuario
+    /// </summary>
+    public class UserPostRequest
+    {
+        /// <summary>
+        /// Propriedade que se refere ao nome do usuario
+        /// </summary>
+        public String UserName{ get; set; }
+
+        /// <summary>
+        /// Propriedade que se refere ao email do usuario
+        /// </summary>
+        public String Email { get; set; }
+
+        /// <summary>
+        /// Propriedade que se refere a senha do usuario
+        /// </summary>
+        public string Password{ get; set; }
+    }
+}
