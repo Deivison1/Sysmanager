d07bbbf330ccd9c92b141a3708d949b6b8a8e035
Deivison1 <santosdeivison480@gmail.com>
2022-06-24 19:18:53 -0300

    Atualizando arquivos

---
+++ Sysmanager/Sysmanager.API.Admin/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sysmanager.Application.Contracts.Users.Request;
+using Sysmanager.Application.Helpers;
+using Sysmanager.Application.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sysmanager.API.Admin.Controllers
+{
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiVersion("1.0")]
+
+    public class AccountController
+    {
+        private readonly UserService _userService;
+        public AccountController(UserService service)
+        {
+            this._userService = service;
+        }
+        [HttpPost("create-account")]
+        public async Task<IActionResult> Post([FromBody] UserPostRequest request)
+        {
+            Console.WriteLine("Inicio do processo");
+            var response = await _userService.PostAsync(request);
+            return Utils.Convert(response);
+        }
+
+    }
+}
---
+++ Sysmanager/Sysmanager.Application/Services/UserService.cs
@@ -0,0 +1,32 @@
+﻿using Sysmanager.Application.Contracts.Users.Request;
+using Sysmanager.Application.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sysmanager.Application.Services
+{
+     public class UserService
+    {
+        public UserService()
+        {
+
+        }
+        public async Task<ResultData> PostAsync(UserPostRequest request)
+        {
+            var erros = new List<string>();
+
+            if (request.UserName == "" || request.UserName == null)
+                erros.Add("Precisa informar a propriedade (UserName)");
+            if (request.Email == "" || request.Email == null)
+                erros.Add("Precisa informar a propriedade (Email)");
+            if (request.Password == "" || request.Password == null)
+                erros.Add("Precisa informar a propriedade (Password)");
+            if (erros.Count > 0)
+                return Utils.ErrorData(erros);
+
+            return Utils.SuccessData(request);
+        }
+    }
+}
