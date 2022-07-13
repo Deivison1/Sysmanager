d53b18dc65a877c87e490290ead7db4f6f924b0c
Deivison1 <santosdeivison480@gmail.com>
2022-07-13 14:38:44 -0300

    criando o PostRequestValidators de category

---
+++ Sysmanager/Sysmanager.Application/Validators/Category/CategoryPostRequestValidator.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using Sysmanager.Application.Contracts.Category.Request;
+using Sysmanager.Application.Data.Mysql.Repositories;
+using Sysmanager.Application.Errors;
+using Sysmanager.Application.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sysmanager.Application.Validators.Category
+{
+    public class CategoryPostRequestValidator : AbstractValidator<CategoryPostRequest>
+    {
+        public CategoryPostRequestValidator(CategoryRepository repository)
+        {
+            RuleFor(contract => contract.Name)
+                .Must(name => !string.IsNullOrEmpty(name))
+                .WithMessage(SysManagerErrors.Category_Post_BadRequest_Name_Cannot_Be_Null_Or_Empty.Description());
+
+            RuleFor(contract => contract.Active)
+                .Must(active => active == true || active == false)
+                .WithMessage(SysManagerErrors.Category_Post_BadRequest_Name_Cannot_Be_Null_Or_Empty.Description());
+
+            RuleFor(contract => contract.Name)
+                .Must(name => {
+                    var exists = repository.GetUnityByNameAsync(name);
+                    return exists != null;
+                })
+                .WithMessage(SysManagerErrors.Category_Post_BadRequest_Name_Cannot_Be_Duplicated.Description());
+        }
+    }
+}
