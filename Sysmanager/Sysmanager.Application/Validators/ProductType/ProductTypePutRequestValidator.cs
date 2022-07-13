3fc1168c20f325fcd8fb1a0707c0edcf98b8c5bf
Deivison1 <santosdeivison480@gmail.com>
2022-07-13 14:55:34 -0300

    Criando a classe ProductTypePutRequestValidators

---
+++ Sysmanager/Sysmanager.Application/Validators/ProductType/ProductTypePutRequestValidator.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using Sysmanager.Application.Contracts.ProductType;
+using Sysmanager.Application.Data.Mysql.Repositories;
+using Sysmanager.Application.Errors;
+using Sysmanager.Application.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sysmanager.Application.Validators.ProductType
+{
+    public class ProductTypePutRequestValidator : AbstractValidator<ProductTypePutRequest>
+    {
+        public ProductTypePutRequestValidator(ProductTypeRepository repository)
+        {
+            RuleFor(contract => contract.Name)
+                .Must(name => !string.IsNullOrEmpty(name))
+                .WithMessage(SysManagerErrors.ProductType_Post_BadRequest_Name_Cannot_Be_Null_Or_Empty.Description());
+
+            RuleFor(contract => contract.Active)
+                .Must(active => active == true || active == false)
+                .WithMessage(SysManagerErrors.ProductType_Post_BadRequest_Name_Cannot_Be_Null_Or_Empty.Description());
+            RuleFor(contract => contract)
+                .Must(x => {
+                    var exists = repository.GetProductTypeByNameAsync(x.Name).Result;
+
+                    if (exists != null)
+                        if (exists.Id != x.Id)
+                            return false;
+
+                    return true;
+                })
+                .WithMessage(SysManagerErrors.ProductType_Post_BadRequest_Name_Cannot_Be_Duplicated.Description());
+        }
+    }
+}
