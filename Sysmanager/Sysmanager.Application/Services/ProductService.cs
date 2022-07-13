4061eec151906d14a662a3dbe8bda87f3aba8646
Deivison1 <santosdeivison480@gmail.com>
2022-07-13 14:23:50 -0300

    Corrindo erros na ProductService

--- Sysmanager/Sysmanager.Application/Services/ProductService.cs
+++ Sysmanager/Sysmanager.Application/Services/ProductService.cs
@@ -1,8 +1,10 @@
 ﻿using Sysmanager.Application.Contracts.Products.Request;
 using Sysmanager.Application.Data.Mysql.Entities;
 using Sysmanager.Application.Data.Mysql.Repositories;
+using Sysmanager.Application.Errors;
 using Sysmanager.Application.Helpers;
 using Sysmanager.Application.Validators.Product;
+using System;
 using System.Threading.Tasks;
 
 namespace Sysmanager.Application.Services
@@ -10,25 +12,74 @@ namespace Sysmanager.Application.Services
     public class ProductService
     {
         private readonly ProductRepository _productRepository;
-        private readonly ProductTypeRepositoty _productTypeRepositoty;
+        private readonly ProductTypeRepository _productTypeRepository;
         private readonly CategoryRepository _categoryRepository;
         private readonly UnityRepository _unityRepository;
-        public ProductService(ProductRepository unityRepository)
+        public ProductService(ProductRepository repository,
+                              ProductTypeRepository productTypeRepository,
+                              CategoryRepository categoryRepository,
+                              UnityRepository unityRepository)
         {
-            this._productRepository = unityRepository;
+            this._productRepository = repository;
+            this._productTypeRepository = productTypeRepository;
+            this._categoryRepository = categoryRepository;
+            this._unityRepository = unityRepository;
         }
 
-        public async Task<ResultData> PostAsync(ProductPostRequest request)
+        public async Task<ResultData> PostAsync(ProductPostRequest product)
         {
-            var validator = new ProductPostRequestValidator(_productRepository);
-            var validatorResult = validator.Validate(request);
+            if (product == null)
+                return Utils.ErrorData(SysManagerErrors.Product_Post_BadRequest_Contract_Cannot_Be_Null.Description());
 
-            if (!validatorResult.IsValid)
-                return Utils.ErrorData(validatorResult.Errors.ToErrorList());
-            var entity = new ProductEntity(request);
-            var response = await _productRepository.CreateProduct(entity);
+            var validator = new ProductPostRequestValidator(_productRepository, _productTypeRepository, _categoryRepository, _unityRepository);
+            var validationResult = validator.Validate(product);
+
+            if (!validationResult.IsValid)
+                return Utils.ErrorData(validationResult.Errors.ToErrorList());
+
+            var entity = new ProductEntity(product);
+            return Utils.SuccessData(await _productRepository.CreateAsync(entity));
+        }
+
+        public async Task<ResultData> PutAsync(ProductPutRequest product)
+        {
+            if (product == null)
+                return Utils.ErrorData(SysManagerErrors.Product_Put_BadRequest_Contract_Cannot_Be_Null.Description());
+
+            var validator = new ProductPutRequestValidator(_productRepository, _productTypeRepository, _categoryRepository, _unityRepository);
+            var validationResult = validator.Validate(product);
+
+            if (!validationResult.IsValid)
+                return Utils.ErrorData(validationResult.Errors.ToErrorList());
+
+            var entity = new ProductEntity(product);
+            return Utils.SuccessData(await _productRepository.UpdateAsync(entity));
+        }
+
+        public async Task<ResultData> GetFilterAsync(ProductGetFilterRequest unity)
+        {
+            var response = await _productRepository.GetProductByFiltersync(unity);
             return Utils.SuccessData(response);
-
         }
+
+        public async Task<ResultData> GetAsync(Guid id)
+        {
+            var response = await _productRepository.GetProductByIdAsync(id);
+            if (response == null)
+                return Utils.ErrorData(SysManagerErrors.Unity_Put_BadRequest_Id_Is_Invalid_Or_Inexistent.Description());
+
+            return Utils.SuccessData(response);
+        }
+
+        public async Task<ResultData> DeleteAsync(Guid id)
+        {
+            var exists = await _productRepository.GetProductByIdAsync(id);
+            if (exists == null)
+                return Utils.ErrorData(SysManagerErrors.Unity_Delete_BadRequest_Id_Is_Invalid_Or_Inexistent.Description());
+
+            var response = await _productRepository.DeleteAsync(id);
+            return Utils.SuccessData(response);
+        }
+
     }
 }
