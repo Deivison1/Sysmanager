86fbd63e6dc2b57db387fb11d63a1d4a553d9f22
Deivison1 <santosdeivison480@gmail.com>
2022-08-04 14:05:06 -0300

    criando classes da models da product-type

---
+++ Sysmanager/Sysmanager.UI.Admin/src/app/services/product-type.service.ts
@@ -0,0 +1,14 @@
+import { Injectable} from '@angular/core';
+import { environment } from '../../environments/environment';
+import { ServiceBase} from '../service-base/service-base';
+import {ProductTypeView} from '../components/product-type/models/product-type.view';
+
+@Injectable()
+export class ProductTypeService extends ServiceBase<ProductTypeView>{
+    constructor(){
+        super
+            ({
+               endpoint: `${environment.url_api}product-type`
+            })
+    }
+}
